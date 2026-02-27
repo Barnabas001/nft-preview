c757eb164c7ae93d9ea3f72416e614e751ffe474
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2026-02-27 14:03:20 +0100

    type declarations and made the card reusable

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,71 @@ import iconEth from "./assets/icon-ethereum.svg";
 import clockIcon from "./assets/icon-clock.svg";
 import imageAvatar from "./assets/image-avatar.png";
 
+type NftCardProps = {
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+  daysLeft: number;
+  creatorName: string;
+  creatoAvatar: string;
+};
+
 export default function App() {
   return (
     <div className="bg-blue-950 flex justify-center screen h-screen items-center">
-      <NftCard />
+      <NftCard
+        image={nftImage}
+        title="Equilibrium #3429"
+        description="Our Equilibrium collection promotes balance and calm."
+        price={0.41}
+        daysLeft={3}
+        creatorName="Jules Wyvern"
+        creatoAvatar={imageAvatar}
+      />
     </div>
   );
 }
 
-function NftCard() {
+function NftCard({
+  image,
+  title,
+  description,
+  price,
+  daysLeft,
+  creatorName,
+  creatoAvatar,
+}: NftCardProps) {
   return (
     <div className="bg-blue-900 rounded-2xl p-6 max-w-sm w-full">
       <div className="relative group rounded-xl overflow-hidden mb-4 cursor-pointer">
-        <img src={nftImage} className="w-full rounded-xl" />
+        <img src={image} className="w-full rounded-xl" />
         <div className="absolute insect-0 bg-cyan-400/50 opacity-0 group-hover:opacity-100 duration-300 flex item-center justify-center">
           <img src={iconView} alt="view icon" />
         </div>
       </div>
-      <h3 className="text-white font-bold text-xl mb-2">Equilibrium #3429</h3>
-      <p className="text-gray-400 text-sm mb-4">
-        Our Equilibrium collection promotes balance and calm.
-      </p>
+      <h3 className="text-white font-bold text-xl mb-2">{title}</h3>
+      <p className="text-gray-400 text-sm mb-4">{description}</p>
 
       <div className="flex justify-between border-b border-gray-700 py-3">
         <div className="flex items-center gap-2">
           <img src={iconEth} className="h-4 w-4 inline" />
-          <p className="text-cyan-400">0.041 ETH</p>
+          <p className="text-cyan-400">{price} ETH</p>
         </div>
 
         <div className="flex items-center gap-2">
           <img src={clockIcon} />
-          <p>3 days left</p>
+          <p>{daysLeft} days left</p>
         </div>
       </div>
 
       <div className="flex items-center gap-3 mt-4">
         <img
-          src={imageAvatar}
+          src={creatoAvatar}
           className="h-8 w-8 rounded-full ring-1 ring-white/20"
         />
         <p className="text-sm text-gray-400">
-          <span className="text-gray-500">Creation of</span> Jules Wyveren
+          <span className="text-gray-500">Creation of</span> {creatorName}
         </p>
       </div>
     </div>
